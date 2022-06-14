2dfe147ebe3040caf8fe70166c69a105fd5ce243
Andreas Levander <andreas.levander92@gmail.com>
2022-06-14 14:35:01 +0300

    added docker compose and analytics

--- deno-jwt/deps.js
+++ deno-jwt/deps.js
@@ -1,1 +1,4 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import * as jose from "https://deno.land/x/jose@v4.8.1/index.ts";
+
+export { jose };
--- deno-jwt/routes.js
+++ deno-jwt/routes.js
@@ -1,31 +1,29 @@
 import { Router } from "./deps.js";
-import * as jose from 'https://deno.land/x/jose@v4.8.1/index.ts';
+import { jose } from "./deps.js";
 
 const router = new Router();
 
-
-const { publicKey, privateKey } = await jose.generateKeyPair('PS256');
+const { publicKey, privateKey } = await jose.generateKeyPair("PS256");
 
 const publicJwk = await jose.exportJWK(publicKey);
 
-const kid = "asd12355435dfs53"
+const kid = "asd12355435dfs53";
 
-publicJwk.kid = kid
+publicJwk.kid = kid;
 
 router.get("/token", async (context) => {
-  const jwt = await new jose.SignJWT({ 'urn:example:claim': true })
-  .setProtectedHeader({ alg: 'PS256', typ: "JWT", kid: kid })
-  .sign(privateKey)
-
+  const jwt = await new jose.SignJWT({ "urn:example:claim": true })
+    .setProtectedHeader({ alg: "PS256", typ: "JWT", kid: kid })
+    .sign(privateKey);
 
   context.response.body = {
-    jwt
+    jwt,
   };
 });
 
 router.get("/verify", (context) => {
   context.response.body = {
-    keys: [publicJwk]
+    keys: [publicJwk],
   };
 });
 
